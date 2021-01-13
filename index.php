a1210fe0bf62cc1fea071f65318e708090b6bd95
Maksim Kiselov <givi.sarapionovich@gmail.com>
2021-01-13 22:55:32 +0200

    Lesson 1 and Leson2

--- index.php
+++ index.php
@@ -5,11 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <style>@import url(css/style.css);</style>
+    <title>NIX Education</title>
 </head>
 <body>
-<?
-echo ("Hello, NIX Education")
-?>
+<header>
+    <div class="container">
+        <a href="/" class="logo">Nixsolution beginer</a>
+        <nav>
+            <ul>
+                <li><a href="">Урок 1</a></li>
+                <li><a href="">Урок 2</a></li>
+                <li><a href="">Урок 3</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="main">
+    <div class="container"><?
+    echo ("Hello, NIX Education")
+    ?>
+    </div>
+</div>
+<footer><div class="container"></div></footer>
 </body>
 </html>
